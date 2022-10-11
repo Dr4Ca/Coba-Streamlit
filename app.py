71b6b64671a38d0b09c8f49e59e088ce233072e5
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-11 17:20:12 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Coba cobaan", page_icon=":tada:", layout="wide")
+
+st.subheader("Halo, namaku Azzam atau bisa dipanggil DR4C4 :wave:")
+st.title("Cuman seorang siswa yang lagi belajar python dan ngerjain proyek buat presentai mei tahun 2023 nanti.")
+st.write("Ya, halaman ini hanyalah percobaan dari projek saya tentang kalkulator rumus, saya masih minim pengetahuan tentang python, jika ada salah mohon dikoreksi.")
